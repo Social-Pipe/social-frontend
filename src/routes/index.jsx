594ff890d5f898bdcae32a5bcfc14952d23eab85
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-05-10 20:10:40 -0300

    add new pages in Routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import ForgetPassword from '../pages/ForgetPassword';
 import Login from '../pages/Login';
+import Register from '../pages/Register';
 
 const Routes = () => (
 	<Router>
 		<Switch>
 			<Route exact path="/" component={Login} />
 			<Route exact path="/forgetPassword" component={ForgetPassword} />
+			<Route exact path="/register" component={Register} />
 		</Switch>
 	</Router>
 );
