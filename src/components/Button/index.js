38837dc9c8f9d010b0aa89f8302ac85f81475949
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-05-10 12:18:23 -0300

    add Button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export default styled.button`
+	height: 3rem;
+	width: 100%;
+	text-align: center;
+	background-color: #1ac863;
+	border: none;
+	border-radius: 8px;
+	font-size: 1rem;
+	color: #fff;
+	font-weight: 500;
+`;
