50098b513a1bb0cd5a707f2378b96f932ade5cbd
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-05-12 12:12:23 -0300

    add validation in forgetPassword page

--- src/pages/ForgetPassword/Form/index.jsx
+++ src/pages/ForgetPassword/Form/index.jsx
@@ -1,31 +1,54 @@
+import { useFormik } from 'formik';
 import { Link } from 'react-router-dom';
 
 import Container from './styles';
 
 import Button from '../../../components/Button';
+import forgetPasswordSchema from '../../../validations/forgetPasswordSchema';
 
-const ForgetPassword = ({ onPressButtonAfter }) => (
-	<Container>
-		<h1>Esqueci minha senha</h1>
-		<p>
-			Preencha o campo abaixo com o seu email cadastrado aqui na Social Pipe
-			para recuperar a sua senha.
-		</p>
-		<form>
-			<fieldset>
-				<label htmlFor="email">Email</label>
-				<input type="text" id="email" />
-			</fieldset>
-			<div className="container_buttons">
-				<div className="container_forget">
-					<Link to="/">Voltar para a tela de login</Link>
+const initialValues = {
+	email: '',
+};
+
+const ForgetPassword = ({ onPressButtonAfter }) => {
+	const formik = useFormik({
+		initialValues,
+		validationSchema: forgetPasswordSchema,
+		onSubmit: values => {
+			console.log(values);
+			onPressButtonAfter();
+		},
+	});
+
+	return (
+		<Container>
+			<h1>Esqueci minha senha</h1>
+			<p>
+				Preencha o campo abaixo com o seu email cadastrado aqui na Social Pipe
+				para recuperar a sua senha.
+			</p>
+			<form onSubmit={formik.handleSubmit}>
+				<fieldset>
+					<label htmlFor="email">Email</label>
+					<input
+						type="text"
+						id="email"
+						name="email"
+						value={formik.values.email}
+						onChange={formik.handleChange}
+					/>
+				</fieldset>
+				<div className="container_buttons">
+					<div className="container_forget">
+						<Link to="/">Voltar para a tela de login</Link>
+					</div>
+					<Button type="submit" className="button">
+						Recuperar senha
+					</Button>
 				</div>
-				<Button type="button" onClick={onPressButtonAfter} className="button">
-					Recuperar senha
-				</Button>
-			</div>
-		</form>
-	</Container>
-);
+			</form>
+		</Container>
+	);
+};
 
 export default ForgetPassword;
