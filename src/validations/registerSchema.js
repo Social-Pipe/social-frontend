3da9d71ff431cfe32c3c3d3cc74e93fb85953353
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-05-13 11:43:27 -0300

    add checkbox validation in registerSchema

--- src/validations/registerSchema.js
+++ src/validations/registerSchema.js
@@ -4,7 +4,8 @@ export default Yup.object().shape({
 	name: Yup.string().required(),
 	email: Yup.string().email().required(),
 	phone: Yup.string()
-		.matches(/(\(\d{2}\)\d{5}-\d{4})|(\(\d{2}\)\d{4}-\d{4})/g)
+		.matches(/(\(\d{2}\)\s\d{5}-\d{4})|(\(\d{2}\)\s\d{4}-\d{4})/g)
 		.required(),
 	password: Yup.string().required(),
+	checkbox: Yup.string().required(),
 });
