54b5bc18b00fd0ab5d6ae33da06d88fa4aaf3545
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-05-14 12:25:04 -0300

    feat(component): add Header Component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import Container from './styles';
+
+import logo from '../../assets/images/logo.png';
+
+const Header = () => (
+	<Container>
+		<img src={logo} alt="logo" />
+	</Container>
+);
+
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export default styled.header`
+	display: flex;
+	align-items: center;
+	padding-left: 3rem;
+	width: 100%;
+	height: 3.75rem;
+	background: #ee4266;
+
+	img {
+		width: 8.62rem;
+		object-fit: contain;
+	}
+`;
