347ae847d1d287639689530f09cc1879042bef35
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-05-18 11:05:07 -0300

    feat(page): add fields of paymentForm

--- src/pages/Register/PaymentForm/index.jsx
+++ src/pages/Register/PaymentForm/index.jsx
@@ -1,21 +1,78 @@
 import Container from './styles';
 
 import Button from '../../../components/Button';
-import { ContainerButtons } from '../styles';
+import maskCardNumber from '../../../utils/maskCardNumber';
+import maskPhone from '../../../utils/maskPhone';
+import { ContainerButtons, FormContainer } from '../styles';
 
-const PaymentForm = () => (
+const PaymentForm = ({ formik }) => (
 	<Container>
 		<h1>Finalize adicionando suas informações de assinatura!</h1>
+		<p>
+			O ambiente é 100% seguro. Não revelaremos a ninguém nenhuma informação de
+			pagamento ou quaisquer outra informação que tenha inserido na Social Pipe.
+		</p>
 		<div>
-			<div className="info_payment_container">
-				<p>Informações de pagamento</p>
-			</div>
-			<ContainerButtons className="container_buttons">
-				<Button type="submit" className="button">
-					Finalizar cadastro
-				</Button>
-				<div className="container_forget" />
-			</ContainerButtons>
+			<FormContainer>
+				<fieldset>
+					<label htmlFor="cardNumber">Número do cartão</label>
+					<input
+						type="text"
+						id="cardNumber"
+						name="cardNumber"
+						onBlur={formik.handleBlur}
+						value={formik.values.cardNumber}
+						onChange={e =>
+							maskCardNumber(e.target.value, newValue =>
+								formik.setFieldValue('cardNumber', newValue)
+							)
+						}
+					/>
+				</fieldset>
+				<fieldset>
+					<label htmlFor="email">Email</label>
+					<input
+						type="email"
+						id="email"
+						name="email"
+						onBlur={formik.handleBlur}
+						value={formik.values.email}
+						onChange={formik.handleChange}
+					/>
+				</fieldset>
+				<fieldset>
+					<label htmlFor="phone">Celular</label>
+					<input
+						type="tel"
+						id="phone"
+						name="phone"
+						value={formik.values.phone}
+						onBlur={formik.handleBlur}
+						onChange={e =>
+							maskPhone(e.target.value, newValue =>
+								formik.setFieldValue('phone', newValue)
+							)
+						}
+					/>
+				</fieldset>
+				<fieldset>
+					<label htmlFor="password">Senha</label>
+					<input
+						type="password"
+						id="password"
+						name="password"
+						onBlur={formik.handleBlur}
+						value={formik.values.password}
+						onChange={formik.handleChange}
+					/>
+				</fieldset>
+				<ContainerButtons className="container_buttons">
+					<Button type="button" className="button">
+						Finalizar cadastro
+					</Button>
+					<div className="container_forget" />
+				</ContainerButtons>
+			</FormContainer>
 		</div>
 	</Container>
 );
--- src/pages/Register/PaymentForm/styles.js
+++ src/pages/Register/PaymentForm/styles.js
@@ -8,20 +8,8 @@ export default styled.div`
 	> div {
 		max-width: 32.37rem;
 
-		.info_payment_container {
-			height: 22.19rem;
-			border: 1px solid #acacac;
-			display: flex;
-			align-items: center;
-			justify-content: center;
-			margin: 1.9rem 0;
-
-			p {
-				color: #acacac;
-				font-weight: 700;
-				font-size: 2.43rem;
-				text-align: center;
-			}
+		.container_buttons {
+			margin-top: 3.5rem;
 		}
 	}
 
