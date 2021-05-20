b3efbdaa535d44c808d2d0b92fef5f68d3dbdc9e
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-05-20 16:21:01 -0300

    feat(page): add NotPayment modal in DashBoard

--- src/pages/DashBoard/index.jsx
+++ src/pages/DashBoard/index.jsx
@@ -1,23 +1,34 @@
+import { useState } from 'react';
+
 import Container from './styles';
 
 import Card from '../../components/Card';
+import NotPaymentAccept from '../../components/NotPaymentAccept';
+import Modal from '../../Container/Modal';
 
-const DashBoard = () => (
-	<Container>
-		<div className="header">
-			<p>Bem vindo(a), Usuário!</p>
-			<h2>Dashboard</h2>
-		</div>
-		<div className="content">
-			<Card />
-			<Card text="Coca Cola" />
-			<Card text="Coca Cola" />
-			<Card text="Coca Cola" />
-			<Card text="Coca Cola" />
-			<Card text="Coca Cola" />
-		</div>
-		<span>Aprovando postagens desde 2021</span>
-	</Container>
-);
+const DashBoard = () => {
+	const [showModal, setShowModal] = useState(false);
+
+	return (
+		<Container>
+			<Modal showModal={showModal} handleOutClick={() => setShowModal(false)}>
+				<NotPaymentAccept />
+			</Modal>
+			<div className="header">
+				<p>Bem vindo(a), Usuário!</p>
+				<h2>Dashboard</h2>
+			</div>
+			<div className="content">
+				<Card />
+				<Card text="Coca Cola" />
+				<Card text="Coca Cola" />
+				<Card text="Coca Cola" />
+				<Card text="Coca Cola" />
+				<Card text="Coca Cola" />
+			</div>
+			<span>Aprovando postagens desde 2021</span>
+		</Container>
+	);
+};
 
 export default DashBoard;
