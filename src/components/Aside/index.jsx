f363a89ae9ef092ca2e4e7a19c08bff93f9cb29c
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-05-20 16:20:12 -0300

    feat(component): add NewClient modal in Aside

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,30 +1,43 @@
+import { useState } from 'react';
 import { AiOutlinePlus } from 'react-icons/ai';
 import { RiArrowRightSLine } from 'react-icons/ri';
 
 import Container from './styles';
 
-const Aside = () => (
-	<Container>
-		<div>
-			<button type="button">
-				<AiOutlinePlus color="#fff" size={16} />
-				<p>Novo Cliente</p>
-			</button>
-			<h2>Meus Clientes</h2>
-			<p>Coca Cola</p>
-		</div>
-		<div>
-			<div className="comment">
-				<div>
-					<span>Você está logado como</span>
-					<p>Usuário Teste</p>
-				</div>
-				<RiArrowRightSLine color="#FCDA7B" size={32} />
-			</div>
+import Modal from '../../Container/Modal';
+import NewClient from '../NewClient';
 
-			<button type="button">Sair</button>
-		</div>
-	</Container>
-);
+const Aside = () => {
+	const [showModal, setShowModal] = useState(false);
+
+	return (
+		<>
+			<Modal showModal={showModal} handleOutClick={() => setShowModal(false)}>
+				<NewClient saveClient={() => setShowModal(false)} />
+			</Modal>
+			<Container>
+				<div>
+					<button type="button" onClick={() => setShowModal(true)}>
+						<AiOutlinePlus color="#fff" size={16} />
+						<p>Novo Cliente</p>
+					</button>
+					<h2>Meus Clientes</h2>
+					<p>Coca Cola</p>
+				</div>
+				<div>
+					<div className="comment">
+						<div>
+							<span>Você está logado como</span>
+							<p>Usuário Teste</p>
+						</div>
+						<RiArrowRightSLine color="#FCDA7B" size={32} />
+					</div>
+
+					<button type="button">Sair</button>
+				</div>
+			</Container>
+		</>
+	);
+};
 
 export default Aside;
