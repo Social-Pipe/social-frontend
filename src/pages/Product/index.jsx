3195d95403e76e9faabf05c2c969709bd0d8e3fa
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-05-20 16:22:55 -0300

    feat(page): add deleteItem and newClient modals in Product

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,47 +1,70 @@
+import { useState } from 'react';
 import { BsFillGearFill } from 'react-icons/bs';
 
 import Container from './styles';
 
 import Button from '../../components/Button';
+import DeleteItem from '../../components/DeleteItem';
+import NewClient from '../../components/NewClient';
 import Row from '../../components/Row';
+import Modal from '../../Container/Modal';
 
-const Product = () => (
-	<Container>
-		<div className="header">
-			<div>
-				<p>Dashboard</p>
-				<h2>Coca Cola</h2>
-			</div>
-			<div className="share">
+const Product = () => {
+	const [showModal, setShowModal] = useState(true);
+	const [showModalEdit, setShowModalEdit] = useState(true);
+	return (
+		<Container>
+			<Modal showModal={showModal} handleOutClick={() => setShowModal(false)}>
+				<DeleteItem
+					handleDeleteItem={() => setShowModal(false)}
+					handleNotDeleteItem={() => setShowModal(false)}
+				/>
+			</Modal>
+			<Modal
+				showModal={showModalEdit}
+				handleOutClick={() => setShowModalEdit(false)}
+			>
+				<NewClient saveClient={() => setShowModalEdit(false)} editClient />
+			</Modal>
+			<div className="header">
 				<div>
-					<p>Link de compartilhamento com o cliente</p>
-					<div className="input_container">
-						<input />
-						<button type="button">copiar link</button>
-					</div>
+					<p>Dashboard</p>
+					<h2>Coca Cola</h2>
 				</div>
-				<button type="button">
-					<BsFillGearFill size={24} color="#fff" />
-				</button>
+				<div className="share">
+					<div>
+						<p>Link de compartilhamento com o cliente</p>
+						<div className="input_container">
+							<input />
+							<button type="button">copiar link</button>
+						</div>
+					</div>
+					<button type="button">
+						<BsFillGearFill size={24} color="#fff" />
+					</button>
+				</div>
 			</div>
-		</div>
-		<div className="content">
-			<div className="container_buttons">
-				<Button>Novo post</Button>
-				<button className="secondary" type="button">
-					Mostrar arquivados
-				</button>
+			<div className="content">
+				<div className="container_buttons">
+					<Button>Novo post</Button>
+					<button className="secondary" type="button">
+						Mostrar arquivados
+					</button>
+				</div>
+				<div className="products">
+					<Row
+						deleteItem={() => setShowModal(true)}
+						editItem={() => setShowModalEdit(true)}
+					/>
+					<Row deleteItem={() => setShowModal(true)} />
+					<Row deleteItem={() => setShowModal(true)} />
+					<Row deleteItem={() => setShowModal(true)} />
+					<Row deleteItem={() => setShowModal(true)} />
+				</div>
 			</div>
-			<div className="products">
-				<Row />
-				<Row />
-				<Row />
-				<Row />
-				<Row />
-			</div>
-		</div>
-		<span>Aprovando postagens desde 2021</span>
-	</Container>
-);
+			<span>Aprovando postagens desde 2021</span>
+		</Container>
+	);
+};
 
 export default Product;
