0134c52c798dd530561eecb7782831e1bc18d089
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-05-21 12:55:14 -0300

    feat(component): add NewPost modal

---
+++ src/components/NewPost/index.jsx
@@ -0,0 +1,586 @@
+import { FaFacebookF, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
+import { IoMdClose } from 'react-icons/io';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+
+import Container from './styles';
+
+import Button from '../Button';
+import PhotoContainer from '../PhotoContainer';
+
+const NewPost = ({ saveClient, editClient = false }) => (
+	<Container>
+		<button type="button" className="close_button" onClick={saveClient}>
+			<IoMdClose size={24} color="#fff" />
+		</button>
+		<div className="header_container">
+			<h3>Novo post</h3>
+		</div>
+		<form>
+			<div className="photo_container">
+				<div className="social_redes">
+					<button
+						type="button"
+						className={`facebook ${'formik.values.facebook' ? 'active' : ''}`}
+						onClick={
+							() => {}
+							// formik.setFieldValue('facebook', !formik.values.facebook)
+						}
+					>
+						<FaFacebookF size={24} color="#fff" />
+					</button>
+					<button
+						type="button"
+						className={`instagram ${
+							'formik.values.instagram ' ? 'active' : ''
+						}`}
+						onClick={
+							() => {}
+							// formik.setFieldValue('instagram', !formik.values.instagram)
+						}
+					>
+						<FaInstagram size={24} color="#fff" />
+					</button>
+					<button
+						type="button"
+						className={`linkedin ${'formik.values.linkedin ' ? 'active' : ''}`}
+						onClick={
+							() => {}
+							// formik.setFieldValue('linkedin', !formik.values.linkedin)
+						}
+					>
+						<FaLinkedinIn size={24} color="#fff" />
+					</button>
+				</div>
+
+				<Button onClick={saveClient} type="button">
+					Salvar novo cliente
+				</Button>
+			</div>
+			<div className="inputs_container">
+				<div className="creating">
+					<p>Criativo</p>
+					<div>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="35.51"
+							height="29.288"
+							viewBox="0 0 35.51 29.288"
+						>
+							<g id="image" opacity="0.96">
+								<g
+									id="Grupo_30"
+									data-name="Grupo 30"
+									transform="translate(0 0)"
+								>
+									<g id="Grupo_29" data-name="Grupo 29">
+										<path
+											id="Caminho_15"
+											data-name="Caminho 15"
+											d="M33.942,44.856H1.568A1.569,1.569,0,0,0,0,46.424V72.576a1.569,1.569,0,0,0,1.568,1.568H33.942a1.569,1.569,0,0,0,1.568-1.568V46.424A1.569,1.569,0,0,0,33.942,44.856Zm0,27.72H1.568V46.424H33.942Z"
+											transform="translate(0 -44.856)"
+											fill="#3c3f4f"
+										/>
+									</g>
+								</g>
+								<g
+									id="Grupo_32"
+									data-name="Grupo 32"
+									transform="translate(3.726 3.726)"
+								>
+									<g id="Grupo_31" data-name="Grupo 31">
+										<path
+											id="Caminho_16"
+											data-name="Caminho 16"
+											d="M81,98.58H54.506a.784.784,0,0,0-.784.784v17.325a.784.784,0,0,0,.784.784H81a.784.784,0,0,0,.784-.784V99.364A.784.784,0,0,0,81,98.58ZM55.29,100.148H80.212v13.27l-4.94-4.94a.784.784,0,0,0-1.109,0l-3.011,3.011-6.905-6.905a.784.784,0,0,0-1.109,0l-7.849,7.848V100.148Zm0,15.757V114.65l8.4-8.4L73.35,115.9Zm24.923,0H75.568L72.261,112.6l2.457-2.457,5.314,5.313a.783.783,0,0,0,.18.134v.316Z"
+											transform="translate(-53.722 -98.58)"
+											fill="#3c3f4f"
+										/>
+									</g>
+								</g>
+								<g
+									id="Grupo_34"
+									data-name="Grupo 34"
+									transform="translate(18.145 6.781)"
+								>
+									<g id="Grupo_33" data-name="Grupo 33">
+										<path
+											id="Caminho_17"
+											data-name="Caminho 17"
+											d="M264.579,142.629a2.964,2.964,0,1,0,2.964,2.964A2.967,2.967,0,0,0,264.579,142.629Zm0,4.359a1.4,1.4,0,1,1,1.4-1.4A1.4,1.4,0,0,1,264.579,146.988Z"
+											transform="translate(-261.616 -142.629)"
+											fill="#3c3f4f"
+										/>
+									</g>
+								</g>
+								<g
+									id="Grupo_36"
+									data-name="Grupo 36"
+									transform="translate(3.726 24.386)"
+								>
+									<g id="Grupo_35" data-name="Grupo 35">
+										<path
+											id="Caminho_18"
+											data-name="Caminho 18"
+											d="M61.324,396.458H54.506a.784.784,0,1,0,0,1.568h6.818a.784.784,0,1,0,0-1.568Z"
+											transform="translate(-53.722 -396.458)"
+											fill="#3c3f4f"
+										/>
+									</g>
+								</g>
+								<g
+									id="Grupo_38"
+									data-name="Grupo 38"
+									transform="translate(14.121 24.386)"
+								>
+									<g id="Grupo_37" data-name="Grupo 37">
+										<path
+											id="Caminho_19"
+											data-name="Caminho 19"
+											d="M206.174,396.458h-1.788a.784.784,0,1,0,0,1.568h1.788a.784.784,0,1,0,0-1.568Z"
+											transform="translate(-203.602 -396.458)"
+											fill="#3c3f4f"
+										/>
+									</g>
+								</g>
+							</g>
+						</svg>
+
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="38.422"
+							height="29.266"
+							viewBox="0 0 38.422 29.266"
+						>
+							<g id="youtube" opacity="0.96">
+								<path
+									id="Caminho_13"
+									data-name="Caminho 13"
+									d="M5.628,29.266H32.793a5.635,5.635,0,0,0,5.628-5.628V5.628A5.635,5.635,0,0,0,32.793,0H5.628A5.635,5.635,0,0,0,0,5.628v18.01A5.635,5.635,0,0,0,5.628,29.266ZM2.251,5.628A3.381,3.381,0,0,1,5.628,2.251H32.793A3.381,3.381,0,0,1,36.17,5.628v18.01a3.381,3.381,0,0,1-3.377,3.377H5.628a3.381,3.381,0,0,1-3.377-3.377Zm0,0"
+									fill="#3c3f4f"
+								/>
+								<path
+									id="Caminho_14"
+									data-name="Caminho 14"
+									d="M181,94.508v15.3l13.545-7.776Zm2.251,3.826,6.716,3.731-6.716,3.856Zm0,0"
+									transform="translate(-167.417 -87.416)"
+									fill="#3c3f4f"
+								/>
+							</g>
+						</svg>
+						<div>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="10.422"
+								height="29.288"
+								viewBox="0 0 10.422 29.288"
+							>
+								<path
+									id="Subtração_2"
+									data-name="Subtração 2"
+									d="M-2026.578-2481.212h-8.854a1.569,1.569,0,0,1-1.568-1.568v-26.152a1.57,1.57,0,0,1,1.568-1.568h8.854v1.568h-8.854v26.152h8.854v1.566Zm0-3.334h-5.912a.785.785,0,0,1-.784-.784.785.785,0,0,1,.784-.784h5.912v1.567Zm0-3.334h-5.912a.785.785,0,0,1-.784-.784v-17.325a.785.785,0,0,1,.784-.784h5.912v1.568h-5.128v12.285l5.128-5.128v2.217l-5.128,5.128v1.255h5.128v1.567Z"
+									transform="translate(2037 2510.5)"
+									fill="#3c3f4f"
+									opacity="0.96"
+								/>
+							</svg>
+
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="35.51"
+								height="29.288"
+								viewBox="0 0 35.51 29.288"
+							>
+								<g id="image" opacity="0.96">
+									<g
+										id="Grupo_30"
+										data-name="Grupo 30"
+										transform="translate(0 0)"
+									>
+										<g id="Grupo_29" data-name="Grupo 29">
+											<path
+												id="Caminho_15"
+												data-name="Caminho 15"
+												d="M33.942,44.856H1.568A1.569,1.569,0,0,0,0,46.424V72.576a1.569,1.569,0,0,0,1.568,1.568H33.942a1.569,1.569,0,0,0,1.568-1.568V46.424A1.569,1.569,0,0,0,33.942,44.856Zm0,27.72H1.568V46.424H33.942Z"
+												transform="translate(0 -44.856)"
+												fill="#3c3f4f"
+											/>
+										</g>
+									</g>
+									<g
+										id="Grupo_32"
+										data-name="Grupo 32"
+										transform="translate(3.726 3.726)"
+									>
+										<g id="Grupo_31" data-name="Grupo 31">
+											<path
+												id="Caminho_16"
+												data-name="Caminho 16"
+												d="M81,98.58H54.506a.784.784,0,0,0-.784.784v17.325a.784.784,0,0,0,.784.784H81a.784.784,0,0,0,.784-.784V99.364A.784.784,0,0,0,81,98.58ZM55.29,100.148H80.212v13.27l-4.94-4.94a.784.784,0,0,0-1.109,0l-3.011,3.011-6.905-6.905a.784.784,0,0,0-1.109,0l-7.849,7.848V100.148Zm0,15.757V114.65l8.4-8.4L73.35,115.9Zm24.923,0H75.568L72.261,112.6l2.457-2.457,5.314,5.313a.783.783,0,0,0,.18.134v.316Z"
+												transform="translate(-53.722 -98.58)"
+												fill="#3c3f4f"
+											/>
+										</g>
+									</g>
+									<g
+										id="Grupo_34"
+										data-name="Grupo 34"
+										transform="translate(18.145 6.781)"
+									>
+										<g id="Grupo_33" data-name="Grupo 33">
+											<path
+												id="Caminho_17"
+												data-name="Caminho 17"
+												d="M264.579,142.629a2.964,2.964,0,1,0,2.964,2.964A2.967,2.967,0,0,0,264.579,142.629Zm0,4.359a1.4,1.4,0,1,1,1.4-1.4A1.4,1.4,0,0,1,264.579,146.988Z"
+												transform="translate(-261.616 -142.629)"
+												fill="#3c3f4f"
+											/>
+										</g>
+									</g>
+									<g
+										id="Grupo_36"
+										data-name="Grupo 36"
+										transform="translate(3.726 24.386)"
+									>
+										<g id="Grupo_35" data-name="Grupo 35">
+											<path
+												id="Caminho_18"
+												data-name="Caminho 18"
+												d="M61.324,396.458H54.506a.784.784,0,1,0,0,1.568h6.818a.784.784,0,1,0,0-1.568Z"
+												transform="translate(-53.722 -396.458)"
+												fill="#3c3f4f"
+											/>
+										</g>
+									</g>
+									<g
+										id="Grupo_38"
+										data-name="Grupo 38"
+										transform="translate(14.121 24.386)"
+									>
+										<g id="Grupo_37" data-name="Grupo 37">
+											<path
+												id="Caminho_19"
+												data-name="Caminho 19"
+												d="M206.174,396.458h-1.788a.784.784,0,1,0,0,1.568h1.788a.784.784,0,1,0,0-1.568Z"
+												transform="translate(-203.602 -396.458)"
+												fill="#3c3f4f"
+											/>
+										</g>
+									</g>
+								</g>
+							</svg>
+						</div>
+					</div>
+				</div>
+				<div className="date">
+					<p>Data de Postagem</p>
+					<div>
+						<svg
+							id="calendar"
+							xmlns="http://www.w3.org/2000/svg"
+							width="22.048"
+							height="22.048"
+							viewBox="0 0 22.048 22.048"
+						>
+							<g id="Grupo_56" data-name="Grupo 56">
+								<g id="Grupo_55" data-name="Grupo 55">
+									<path
+										id="Caminho_20"
+										data-name="Caminho 20"
+										d="M19.465,1.723H18.431V0H16.709V1.723H5.34V0H3.617V1.723H2.584A2.587,2.587,0,0,0,0,4.306V19.465a2.587,2.587,0,0,0,2.584,2.584H19.465a2.587,2.587,0,0,0,2.584-2.584V4.306A2.587,2.587,0,0,0,19.465,1.723Zm.861,17.742a.862.862,0,0,1-.861.861H2.584a.862.862,0,0,1-.861-.861V8.1h18.6Zm0-13.091H1.723V4.306a.862.862,0,0,1,.861-.861H3.617V5.168H5.34V3.445H16.709V5.168h1.723V3.445h1.034a.862.862,0,0,1,.861.861Z"
+										fill="#3c3f4f"
+									/>
+								</g>
+							</g>
+							<g
+								id="Grupo_58"
+								data-name="Grupo 58"
+								transform="translate(3.289 9.866)"
+							>
+								<g
+									id="Grupo_57"
+									data-name="Grupo 57"
+									transform="translate(0 0)"
+								>
+									<rect
+										id="Retângulo_61"
+										data-name="Retângulo 61"
+										width="1.706"
+										height="1.706"
+										fill="#3c3f4f"
+									/>
+								</g>
+							</g>
+							<g
+								id="Grupo_60"
+								data-name="Grupo 60"
+								transform="translate(6.7 9.866)"
+							>
+								<g
+									id="Grupo_59"
+									data-name="Grupo 59"
+									transform="translate(0 0)"
+								>
+									<rect
+										id="Retângulo_62"
+										data-name="Retângulo 62"
+										width="1.706"
+										height="1.706"
+										fill="#3c3f4f"
+									/>
+								</g>
+							</g>
+							<g
+								id="Grupo_62"
+								data-name="Grupo 62"
+								transform="translate(10.111 9.866)"
+							>
+								<g
+									id="Grupo_61"
+									data-name="Grupo 61"
+									transform="translate(0 0)"
+								>
+									<rect
+										id="Retângulo_63"
+										data-name="Retângulo 63"
+										width="1.827"
+										height="1.706"
+										fill="#3c3f4f"
+									/>
+								</g>
+							</g>
+							<g
+								id="Grupo_64"
+								data-name="Grupo 64"
+								transform="translate(13.644 9.866)"
+							>
+								<g
+									id="Grupo_63"
+									data-name="Grupo 63"
+									transform="translate(0 0)"
+								>
+									<rect
+										id="Retângulo_64"
+										data-name="Retângulo 64"
+										width="1.706"
+										height="1.706"
+										fill="#3c3f4f"
+									/>
+								</g>
+							</g>
+							<g
+								id="Grupo_66"
+								data-name="Grupo 66"
+								transform="translate(17.055 9.866)"
+							>
+								<g
+									id="Grupo_65"
+									data-name="Grupo 65"
+									transform="translate(0 0)"
+								>
+									<rect
+										id="Retângulo_65"
+										data-name="Retângulo 65"
+										width="1.706"
+										height="1.706"
+										fill="#3c3f4f"
+									/>
+								</g>
+							</g>
+							<g
+								id="Grupo_68"
+								data-name="Grupo 68"
+								transform="translate(3.289 13.399)"
+							>
+								<g
+									id="Grupo_67"
+									data-name="Grupo 67"
+									transform="translate(0 0)"
+								>
+									<rect
+										id="Retângulo_66"
+										data-name="Retângulo 66"
+										width="1.706"
+										height="1.706"
+										fill="#3c3f4f"
+									/>
+								</g>
+							</g>
+							<g
+								id="Grupo_70"
+								data-name="Grupo 70"
+								transform="translate(6.7 13.399)"
+							>
+								<g
+									id="Grupo_69"
+									data-name="Grupo 69"
+									transform="translate(0 0)"
+								>
+									<rect
+										id="Retângulo_67"
+										data-name="Retângulo 67"
+										width="1.706"
+										height="1.706"
+										fill="#3c3f4f"
+									/>
+								</g>
+							</g>
+							<g
+								id="Grupo_72"
+								data-name="Grupo 72"
+								transform="translate(10.111 13.399)"
+							>
+								<g
+									id="Grupo_71"
+									data-name="Grupo 71"
+									transform="translate(0 0)"
+								>
+									<rect
+										id="Retângulo_68"
+										data-name="Retângulo 68"
+										width="1.827"
+										height="1.706"
+										fill="#3c3f4f"
+									/>
+								</g>
+							</g>
+							<g
+								id="Grupo_74"
+								data-name="Grupo 74"
+								transform="translate(13.644 13.399)"
+							>
+								<g
+									id="Grupo_73"
+									data-name="Grupo 73"
+									transform="translate(0 0)"
+								>
+									<rect
+										id="Retângulo_69"
+										data-name="Retângulo 69"
+										width="1.706"
+										height="1.706"
+										fill="#3c3f4f"
+									/>
+								</g>
+							</g>
+							<g
+								id="Grupo_76"
+								data-name="Grupo 76"
+								transform="translate(3.289 16.81)"
+							>
+								<g
+									id="Grupo_75"
+									data-name="Grupo 75"
+									transform="translate(0 0)"
+								>
+									<rect
+										id="Retângulo_70"
+										data-name="Retângulo 70"
+										width="1.706"
+										height="1.706"
+										fill="#3c3f4f"
+									/>
+								</g>
+							</g>
+							<g
+								id="Grupo_78"
+								data-name="Grupo 78"
+								transform="translate(6.7 16.81)"
+							>
+								<g
+									id="Grupo_77"
+									data-name="Grupo 77"
+									transform="translate(0 0)"
+								>
+									<rect
+										id="Retângulo_71"
+										data-name="Retângulo 71"
+										width="1.706"
+										height="1.706"
+										fill="#3c3f4f"
+									/>
+								</g>
+							</g>
+							<g
+								id="Grupo_80"
+								data-name="Grupo 80"
+								transform="translate(10.111 16.81)"
+							>
+								<g
+									id="Grupo_79"
+									data-name="Grupo 79"
+									transform="translate(0 0)"
+								>
+									<rect
+										id="Retângulo_72"
+										data-name="Retângulo 72"
+										width="1.827"
+										height="1.706"
+										fill="#3c3f4f"
+									/>
+								</g>
+							</g>
+							<g
+								id="Grupo_82"
+								data-name="Grupo 82"
+								transform="translate(13.644 16.81)"
+							>
+								<g
+									id="Grupo_81"
+									data-name="Grupo 81"
+									transform="translate(0 0)"
+								>
+									<rect
+										id="Retângulo_73"
+										data-name="Retângulo 73"
+										width="1.706"
+										height="1.706"
+										fill="#3c3f4f"
+									/>
+								</g>
+							</g>
+							<g
+								id="Grupo_84"
+								data-name="Grupo 84"
+								transform="translate(17.055 13.399)"
+							>
+								<g
+									id="Grupo_83"
+									data-name="Grupo 83"
+									transform="translate(0 0)"
+								>
+									<rect
+										id="Retângulo_74"
+										data-name="Retângulo 74"
+										width="1.706"
+										height="1.706"
+										fill="#3c3f4f"
+									/>
+								</g>
+							</g>
+						</svg>
+
+						<p>11 de Abril de 2021</p>
+					</div>
+				</div>
+				<div className="select">
+					<p>Mostrar para o cliente?</p>
+					<div>
+						<div className="mask">
+							<p>Sim</p>
+							<MdKeyboardArrowDown size={32} color="#717171" />
+						</div>
+						<select>
+							<option>sim</option>
+						</select>
+					</div>
+				</div>
+			</div>
+			<div className="text">
+				<label>Legenda</label>
+				<textarea />
+			</div>
+		</form>
+	</Container>
+);
+
+export default NewPost;
---
+++ src/components/NewPost/styles.js
@@ -0,0 +1,265 @@
+import styled from 'styled-components';
+
+export default styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	height: 100%;
+	position: relative;
+
+	max-width: 54.3rem;
+	max-height: 34.75rem;
+
+	.close_button {
+		position: absolute;
+		top: -2rem;
+		right: 1rem;
+		background: none;
+		border: none;
+		z-index: 2;
+	}
+
+	.header_container {
+		margin-top: 2rem;
+		margin-left: 2rem;
+		margin-right: 2rem;
+
+		margin-bottom: 1rem;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		h3 {
+			font-size: 2.44rem;
+
+			color: #3c3f4f;
+		}
+		button {
+			max-width: 15.5rem;
+		}
+	}
+
+	form {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+
+		display: flex;
+		margin: 0 2rem;
+		padding-top: 2.5rem;
+		padding-left: 2.5rem;
+		padding-right: 2.5rem;
+		padding-bottom: 3rem;
+		border-top: 1px solid #bbbbbb;
+
+		p,
+		label {
+			color: #3c3f4f;
+			font-weight: 500;
+			margin-bottom: 0.62rem;
+		}
+
+		.photo_container {
+			display: flex;
+			justify-content: space-between;
+			.social_redes {
+				display: flex;
+				button {
+					display: flex;
+					align-items: center;
+					justify-content: center;
+					width: 2.25rem;
+					height: 2.25rem;
+					border-radius: 100%;
+					background-color: #dfdfdf;
+					border: none;
+					cursor: pointer;
+
+					&.active {
+						&.facebook,
+						&.linkedin {
+							background: #3b5998;
+						}
+
+						&.instagram {
+							background-color: #8d4b26;
+						}
+					}
+
+					:nth-child(2n) {
+						margin: 0 1rem;
+					}
+				}
+			}
+
+			> button {
+				width: 15.5rem;
+			}
+		}
+		.inputs_container {
+			display: flex;
+			margin: 2.2rem 0;
+
+			> div {
+				flex: 1;
+				justify-content: center;
+			}
+
+			.date {
+				flex: 1.3;
+				display: flex;
+				padding-left: 3.125rem;
+				padding-right: 1.75rem;
+				flex-direction: column;
+
+				> div {
+					display: flex;
+					align-items: center;
+					height: 3rem;
+					border-radius: 8px;
+					border: 1px solid #e4e4e4;
+					box-shadow: 0 15px 40px rgba(183, 183, 183, 0.11);
+					padding-left: 1rem;
+					align-self: stretch;
+
+					p {
+						color: #5d5d5d;
+						font-weight: 500;
+						margin: 0;
+						margin-left: 1rem;
+					}
+				}
+			}
+
+			.select {
+				> div {
+					position: relative;
+
+					.mask {
+						align-items: center;
+						display: flex;
+						height: 3rem;
+						border-radius: 8px;
+						border: 1px solid #e4e4e4;
+						box-shadow: 0 15px 40px rgba(183, 183, 183, 0.11);
+						padding-left: 1rem;
+						align-self: stretch;
+						position: relative;
+						justify-content: space-between;
+						padding: 0 1rem;
+						z-index: 2;
+						pointer-events: none;
+						background: #fff;
+						p {
+							margin: 0;
+						}
+					}
+					select {
+						cursor: pointer;
+						position: absolute;
+						outline: none;
+						top: 0;
+						left: 0;
+						width: 100%;
+						height: 100%;
+						border-radius: 8px;
+						border: none;
+					}
+				}
+			}
+
+			.creating {
+				display: flex;
+				flex-direction: column;
+				> div {
+					display: flex;
+					justify-content: space-between;
+
+					> div {
+						position: relative;
+						padding-left: 0.5rem;
+						svg:first-child {
+							position: absolute;
+							top: 0;
+							right: 2.4rem;
+						}
+						svg:last-child {
+							z-index: 2;
+						}
+					}
+				}
+			}
+
+			fieldset {
+				display: flex;
+				flex-direction: column;
+				border: none;
+
+				:not(:first-child) {
+					margin-top: 1.9rem;
+				}
+				position: relative;
+
+				label {
+					color: #5d5d5d;
+					font-size: 1rem;
+					font-weight: 600;
+				}
+
+				input {
+					width: 100%;
+					margin-top: 1rem;
+					border: 1px solid #e4e4e4;
+					height: 3rem;
+					border-radius: 8px;
+					outline: none;
+					padding-left: 1.5rem;
+					font-size: 1rem;
+					color: #5d5d5d;
+					font-weight: 500;
+					box-shadow: 0px 15px 40px rgba(183, 183, 183, 0.1);
+				}
+			}
+		}
+		.text {
+			display: flex;
+			flex: 1;
+			flex-direction: column;
+
+			textarea {
+				box-shadow: 0 15px 40px rgba(183, 183, 183, 0.11);
+				height: 100%;
+				border-radius: 8px;
+				resize: none;
+				border: 1px solid #e4e4e4;
+				outline: none;
+				padding: 1rem;
+				color: #5d5d5d;
+				font-size: 1rem;
+			}
+		}
+		&.footer {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			flex: 1;
+			background: #e6eef3;
+			border-radius: 0 0 8px 8px;
+			border: 1px solid rgba(190, 190, 190, 0.23);
+			padding: 0 3.75rem;
+
+			p {
+				font-size: 1.19rem;
+				max-width: 25.37rem;
+				font-weight: 500;
+				color: #3c3f4f;
+			}
+
+			strong {
+				font-weight: 600;
+			}
+			button {
+				max-width: 15.5rem;
+			}
+		}
+	}
+`;
