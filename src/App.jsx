192b43763933959c6e5cd48c67ddf3598d645f1f
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-05-22 11:57:08 -0300

    feat(context): add context Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Routes from './routes';
+import ContextProvider from './services/context';
 import GlobalStyles from './theme/globalStyles';
 
 function App() {
 	return (
-		<>
+		<ContextProvider>
 			<GlobalStyles />
 			<div className="App">
 				<Routes />
 			</div>
-		</>
+		</ContextProvider>
 	);
 }
 
---
+++ src/services/context.jsx
@@ -0,0 +1,44 @@
+import { createContext, useEffect, useState, useCallback } from 'react';
+
+export const Context = createContext({
+	menuOpen: true,
+	toggleOpenMenu() {},
+	smart: true,
+});
+
+const ContextProvider = ({ children }) => {
+	const [menuOpen, setMenuOpen] = useState(false);
+	const [smart, setSmart] = useState(false);
+
+	const verifyWidthAndSetNumberSlides = useCallback(width => {
+		if (width <= 800) {
+			setSmart(true);
+			return;
+		}
+
+		setSmart(false);
+	}, []);
+
+	useEffect(() => {
+		verifyWidthAndSetNumberSlides(window.innerWidth);
+		window.addEventListener('resize', () => {
+			verifyWidthAndSetNumberSlides(window.innerWidth);
+		});
+
+		return window.removeEventListener('resize', () => {
+			verifyWidthAndSetNumberSlides(window.innerWidth);
+		});
+	}, []);
+
+	function toggleOpenMenu() {
+		setMenuOpen(!menuOpen);
+	}
+
+	return (
+		<Context.Provider value={{ menuOpen, toggleOpenMenu, smart }}>
+			{children}
+		</Context.Provider>
+	);
+};
+
+export default ContextProvider;
