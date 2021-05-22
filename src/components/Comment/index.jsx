bc9810d1268207681b9d63f3c6819048b85d3011
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-05-22 11:15:09 -0300

    feat(component): add comment for comment area in ratingPost

---
+++ src/components/Comment/index.jsx
@@ -0,0 +1,13 @@
+import Container from './styles';
+
+const Comment = () => (
+	<Container>
+		<header>
+			<h4>Cliente</h4>
+			<span>Seg, 12 de novembro às 21h35</span>
+		</header>
+		<p>{'Muito legal gostei muito do post! <3'}</p>
+	</Container>
+);
+
+export default Comment;
