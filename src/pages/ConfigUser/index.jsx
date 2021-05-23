69bed8e57652dd5285bd265998fe2456ab956531
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-05-23 11:08:57 -0300

    feat(page): add new inputs in configUser

--- src/pages/ConfigUser/index.jsx
+++ src/pages/ConfigUser/index.jsx
@@ -1,7 +1,7 @@
 import { RiArrowLeftSLine } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
 
-import Container from './styles';
+import Container, { FieldSet, Info } from './styles';
 
 import Button from '../../components/Button';
 
@@ -17,51 +17,149 @@ const ConfigUser = () => (
 			</div>
 			<div className="content">
 				<form>
-					<fieldset>
-						<label htmlFor="Name">Nome</label>
-						<input
-							type="text"
-							id="Name"
-							name="Name"
-							// onChange={formik.handleChange}
-							// value={formik.values.email}
-						/>
-					</fieldset>
-					<fieldset>
-						<label htmlFor="email">Email</label>
-						<input
-							type="email"
-							id="email"
-							name="email"
-							// onChange={formik.handleChange}
-							// value={formik.values.email}
-						/>
-					</fieldset>
+					<div>
+						<div className="column">
+							<h2>Informações de cadastro</h2>
+							<FieldSet>
+								<label htmlFor="Name">Nome</label>
+								<input
+									type="text"
+									id="Name"
+									name="Name"
+									// onChange={formik.handleChange}
+									// value={formik.values.email}
+								/>
+							</FieldSet>
+							<FieldSet>
+								<label htmlFor="email">Email</label>
+								<input
+									type="email"
+									id="email"
+									name="email"
+									// onChange={formik.handleChange}
+									// value={formik.values.email}
+								/>
+							</FieldSet>
 
-					<fieldset>
-						<label htmlFor="phone">Celular</label>
-						<input
-							type="tel"
-							id="phone"
-							name="phone"
-							// value={formik.values.phone}
-							// onBlur={formik.handleBlur}
-							// onChange={e => maskPhone(e.target.value)}
-						/>
-					</fieldset>
-					<fieldset>
-						<label htmlFor="password">Senha</label>
-						<input
-							type="password"
-							id="password"
-							name="password"
-							// onChange={formik.handleChange}
-							// value={formik.values.password}
-						/>
-					</fieldset>
+							<FieldSet>
+								<label htmlFor="phone">Celular</label>
+								<input
+									type="tel"
+									id="phone"
+									name="phone"
+									// value={formik.values.phone}
+									// onBlur={formik.handleBlur}
+									// onChange={e => maskPhone(e.target.value)}
+								/>
+							</FieldSet>
+							<FieldSet>
+								<label htmlFor="password">CPF</label>
+								<input
+									id="password"
+									name="password"
+									// onChange={formik.handleChange}
+									// value={formik.values.password}
+								/>
+							</FieldSet>
+							<FieldSet>
+								<label htmlFor="password">Senha</label>
+								<input
+									type="password"
+									id="password"
+									name="password"
+									// onChange={formik.handleChange}
+									// value={formik.values.password}
+								/>
+							</FieldSet>
+						</div>
+						<div className="column">
+							<h2>Endereço de cobrança</h2>
+							<FieldSet>
+								<label htmlFor="Cep">Cep</label>
+								<input
+									id="Cep"
+									name="Cep"
+									// onChange={formik.handleChange}
+									// value={formik.values.password}
+								/>
+							</FieldSet>
+							<FieldSet>
+								<label htmlFor="Adress">Endereço</label>
+								<input
+									id="Adress"
+									name="Adress"
+									// onChange={formik.handleChange}
+									// value={formik.values.password}
+								/>
+							</FieldSet>
+							<div>
+								<FieldSet>
+									<label htmlFor="Num">Numero</label>
+									<input
+										id="Num"
+										name="Num"
+										// onChange={formik.handleChange}
+										// value={formik.values.password}
+									/>
+								</FieldSet>
+								<FieldSet>
+									<label htmlFor="state">Estado</label>
+									<input
+										id="state"
+										name="state"
+										// onChange={formik.handleChange}
+										// value={formik.values.password}
+									/>
+								</FieldSet>
+							</div>
+							<div>
+								<FieldSet>
+									<label htmlFor="city">Cidade</label>
+									<input
+										id="city"
+										name="city"
+										// onChange={formik.handleChange}
+										// value={formik.values.password}
+									/>
+								</FieldSet>
+								<FieldSet>
+									<label htmlFor="Bairro">Bairro</label>
+									<input
+										id="Bairro"
+										name="Bairro"
+										// onChange={formik.handleChange}
+										// value={formik.values.password}
+									/>
+								</FieldSet>
+							</div>
+							<div className="contact">
+								<h3>Informações de contato</h3>
+								<div>
+									<FieldSet>
+										<label htmlFor="DDD">DDD</label>
+										<input
+											id="DDD"
+											name="DDD"
+											// onChange={formik.handleChange}
+											// value={formik.values.password}
+										/>
+									</FieldSet>
+									<FieldSet>
+										<label htmlFor="phone2">Número de celular</label>
+										<input
+											id="phone2"
+											name="phone2"
+											// onChange={formik.handleChange}
+											// value={formik.values.password}
+										/>
+									</FieldSet>
+								</div>
+							</div>
+						</div>
+					</div>
 					<Button secondary>Alterar informações</Button>
 				</form>
-				<div className="info">
+				<Info>
 					<h3>Valor atual de assinatura</h3>
 					<p className="price">R$ 9,90</p>
 					<h3>Histórico de Pagamentos</h3>
@@ -96,7 +194,7 @@ const ConfigUser = () => (
 							Desativar conta
 						</button>
 					</div>
-				</div>
+				</Info>
 			</div>
 
 			<span>Aprovando postagens desde 2021</span>
--- src/pages/ConfigUser/styles.js
+++ src/pages/ConfigUser/styles.js
@@ -40,140 +40,76 @@ export default styled.main`
 
 		.content {
 			display: flex;
-			justify-content: space-between;
+			flex-direction: column;
 			padding-bottom: 4rem;
 		}
 
 		form {
+			display: flex;
+			flex-direction: column;
 			flex: 1;
 			align-self: stretch;
-			min-width: 28.5rem;
-			margin-right: 11.25rem;
+			margin-bottom: 10.3rem;
+
+			button {
+				align-self: center;
+			}
+			> div {
+				display: flex;
+				justify-content: space-between;
+				.column {
+					flex: 1;
+					h2 {
+						font-size: 1.25rem;
+						color: #3c3f4f;
+						font-weight: 700;
+						margin-bottom: 2.75rem;
+					}
+
+					h3 {
+						font-size: 1.25rem;
+						color: #3c3f4f;
+						font-weight: bold;
+						margin-bottom: 1.75rem;
+					}
+					.contact {
+						display: flex;
+						flex-direction: column;
+						margin-top: 2.56rem;
+						> div {
+							display: flex;
+							fieldset:first-child {
+								max-width: 4.25rem;
+							}
+						}
+					}
+					:first-child {
+						margin-right: 10rem;
+						fieldset {
+							margin-bottom: 1.9rem;
+						}
+					}
+
+					> div {
+						display: flex;
+						fieldset {
+							flex: 1;
+
+							:first-child {
+								margin-right: 1rem;
+							}
+						}
+					}
+				}
+			}
 
 			button {
 				max-width: 15.5rem;
 				margin-top: 2rem;
 			}
 		}
-
-		.info {
-			max-width: 34.87rem;
-			width: 100%;
-			display: flex;
-			flex-direction: column;
-			h3 {
-				font-size: 1.25rem;
-				font-weight: bold;
-				color: #3c3f4f;
-			}
-
-			.price {
-				font-size: 2.63rem;
-				font-weight: bold;
-				color: #3c3f4f;
-				margin-top: 0.5rem;
-				margin-bottom: 2.19rem;
-			}
-
-			.history {
-				margin-top: 2.75rem;
-				margin-bottom: 3.75rem;
-
-				> div {
-					display: flex;
-					align-items: center;
-					margin-bottom: 1.25rem;
-
-					p {
-						color: #5d5d5d;
-					}
-					strong {
-						font-weight: bold;
-					}
-
-					span {
-						flex: 1;
-						border: 1px solid #e1e1e1;
-						margin: 0 1rem;
-					}
-
-					div {
-						padding: 0.6rem;
-						background-color: #30a22c;
-						color: #fff;
-						border-radius: 8px;
-						font-weight: bold;
-						font-size: 0.62rem;
-					}
-				}
-			}
-
-			.container_buttons {
-				display: flex;
-				margin-top: auto;
-
-				button {
-					display: flex;
-					align-items: center;
-					justify-content: center;
-					padding: 1rem;
-					font-size: 0.81rem;
-					font-weight: 500;
-					border: none;
-					background: none;
-					cursor: pointer;
-					width: 100%;
-					max-width: 17.25rem;
-
-					&.edit {
-						background-color: #f9f9f9;
-						color: #5d5d5d;
-						border-radius: 8px;
-						margin-right: 1rem;
-					}
-
-					&.desactive {
-						color: #ee4266;
-					}
-				}
-			}
-		}
 	}
 
-	fieldset {
-		display: flex;
-		flex-direction: column;
-		border: none;
-
-		:not(:first-child) {
-			margin-top: 1.9rem;
-		}
-
-		label {
-			color: #5d5d5d;
-			font-size: 0.75rem;
-		}
-
-		input {
-			width: 100%;
-			margin-top: 1rem;
-			border: 1px solid #e4e4e4;
-			height: 3rem;
-			border-radius: 8px;
-			outline: none;
-			padding-left: 1.5rem;
-			font-size: 1rem;
-			color: #5d5d5d;
-			font-weight: 500;
-			box-shadow: 0px 15px 40px rgba(183, 183, 183, 0.1);
-			background: none;
-
-			:-webkit-autofill {
-				background: none;
-				color: #5d5d5d;
-			}
-		}
-	}
 	span {
 		display: block;
 		font-size: 1.25rem;
@@ -187,19 +123,12 @@ export default styled.main`
 	@media (max-width: 1400px) {
 		.container {
 			width: 100%;
-			form {
-				min-width: 12.5rem;
-				max-width: 28.5rem;
-				margin-right: 7.65rem;
-			}
 		}
 	}
 
 	@media (max-width: 800px) {
 		.container .content {
 			form {
-				min-width: 12.5rem;
-				max-width: 100%;
 				margin-right: 0;
 				margin-bottom: 3rem;
 			}
@@ -209,7 +138,127 @@ export default styled.main`
 
 	@media (max-width: 500px) {
 		padding: 2rem;
-		.container .info .container_buttons {
+	}
+`;
+
+export const Form = styled.form``;
+
+export const FieldSet = styled.fieldset`
+	display: flex;
+	flex-direction: column;
+	border: none;
+
+	margin-bottom: 0.87rem;
+
+	label {
+		color: #5d5d5d;
+		font-size: 0.75rem;
+	}
+
+	input {
+		width: 100%;
+		margin-top: 1rem;
+		border: 1px solid #e4e4e4;
+		height: 3rem;
+		border-radius: 8px;
+		outline: none;
+		padding-left: 1.5rem;
+		font-size: 1rem;
+		color: #5d5d5d;
+		font-weight: 500;
+		box-shadow: 0px 15px 40px rgba(183, 183, 183, 0.1);
+		background: none;
+
+		:-webkit-autofill {
+			background: none;
+			color: #5d5d5d;
+		}
+	}
+`;
+
+export const Info = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	h3 {
+		font-size: 1.25rem;
+		font-weight: bold;
+		color: #3c3f4f;
+	}
+
+	.price {
+		font-size: 2.63rem;
+		font-weight: bold;
+		color: #3c3f4f;
+		margin-top: 0.5rem;
+		margin-bottom: 2.19rem;
+	}
+
+	.history {
+		margin-top: 2.75rem;
+		margin-bottom: 3.75rem;
+
+		> div {
+			display: flex;
+			align-items: center;
+			margin-bottom: 1.25rem;
+
+			p {
+				color: #5d5d5d;
+			}
+			strong {
+				font-weight: bold;
+			}
+
+			span {
+				flex: 1;
+				border: 1px solid #e1e1e1;
+				margin: 0 1rem;
+			}
+
+			div {
+				padding: 0.6rem;
+				background-color: #30a22c;
+				color: #fff;
+				border-radius: 8px;
+				font-weight: bold;
+				font-size: 0.62rem;
+			}
+		}
+	}
+
+	.container_buttons {
+		display: flex;
+		margin-top: auto;
+		align-self: center;
+
+		button {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			padding: 1rem;
+			font-size: 0.81rem;
+			font-weight: 500;
+			border: none;
+			background: none;
+			cursor: pointer;
+			max-width: 17.31rem;
+
+			&.edit {
+				background-color: #f9f9f9;
+				color: #5d5d5d;
+				border-radius: 8px;
+				margin-right: 1rem;
+			}
+
+			&.desactive {
+				color: #ee4266;
+			}
+		}
+	}
+
+	@media (max-width: 500px) {
+		.container_buttons {
 			flex-direction: column;
 
 			button {
