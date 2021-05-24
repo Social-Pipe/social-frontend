b1e9e1ab88623fdf32207fa7a3495500e4050b25
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-05-24 17:20:16 -0300

    feat(validations): add configUserSchema

---
+++ src/validations/configUserSchema.js
@@ -0,0 +1,19 @@
+import * as Yup from 'yup';
+
+export default Yup.object().shape({
+	name: Yup.string(),
+	email: Yup.string().email(),
+	phone: Yup.string().matches(
+		/(\(\d{2}\)\s\d{5}-\d{4})|(\(\d{2}\)\s\d{4}-\d{4})/g
+	),
+	cpf: Yup.string().matches(/^(\d{3})\.(\d{3})\.(\d{3})-(\d{2})/g),
+	password: Yup.string(),
+	cep: Yup.string(),
+	adress: Yup.string(),
+	number: Yup.string(),
+	city: Yup.string(),
+	state: Yup.string(),
+	bairro: Yup.string(),
+	ddd: Yup.string().min(2).max(2),
+	phoneContact: Yup.string(),
+});
