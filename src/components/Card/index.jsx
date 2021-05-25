bef4b13b6c5bf907f94b631b31c2306fcf4eb5a1
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-05-25 18:09:31 -0300

    feat(component): change tag div to tag Link in Card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,8 +4,8 @@ import Container from './styles';
 
 import imageTest from '../../assets/images/Coca.png';
 
-const Card = ({ text }) => (
-	<Container existProduct={text}>
+const Card = ({ text, id }) => (
+	<Container to={`dashboard/:${id}/detail`} existProduct={text}>
 		{text ? (
 			<>
 				<img src={imageTest} alt="logo" />
--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,6 +1,8 @@
+import { Link } from 'react-router-dom';
 import styled, { css } from 'styled-components';
 
-export default styled.div`
+export default styled(Link)`
+	text-decoration: none;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
