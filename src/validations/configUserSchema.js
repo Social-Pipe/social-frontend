213c7e52b615bbbec27680aae8fe6fcbe7950d5c
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-05-25 14:55:47 -0300

    feat(validation): add regex cpf,phoneContact in configUserSchema

--- src/validations/configUserSchema.js
+++ src/validations/configUserSchema.js
@@ -6,14 +6,14 @@ export default Yup.object().shape({
 	phone: Yup.string().matches(
 		/(\(\d{2}\)\s\d{5}-\d{4})|(\(\d{2}\)\s\d{4}-\d{4})/g
 	),
-	cpf: Yup.string().matches(/^(\d{3})\.(\d{3})\.(\d{3})-(\d{2})/g),
+	cpf: Yup.string().matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}/g),
 	password: Yup.string(),
-	cep: Yup.string(),
+	cep: Yup.string(/^\d{5}-\d{3}/g),
 	adress: Yup.string(),
 	number: Yup.string(),
 	city: Yup.string(),
 	state: Yup.string(),
 	bairro: Yup.string(),
 	ddd: Yup.string().min(2).max(2),
-	phoneContact: Yup.string(),
+	phoneContact: Yup.string(/(\d{5}-\d{4})|(\d{4}-\d{4})/g),
 });
