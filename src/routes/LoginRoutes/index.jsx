1cd10c4c71d34506d1cdac2193426599dae86e0b
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-06-02 13:12:16 -0300

    feat(routes): add get token of localStorage and loading state
    while tokenStorage not was verify, the screen of loading not will available

--- src/routes/LoginRoutes/index.jsx
+++ src/routes/LoginRoutes/index.jsx
@@ -1,4 +1,5 @@
-import { Switch, Route, useRouteMatch } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
+import { Switch, Route, useRouteMatch, useHistory } from 'react-router-dom';
 
 import ContainerLogin from './styles';
 
@@ -7,28 +8,57 @@ import DesactiveSucess from '../../pages/DesactiveSucess';
 import ForgetPassword from '../../pages/ForgetPassword';
 import Login from '../../pages/Login';
 import Register from '../../pages/Register';
+import { Context } from '../../services/context';
 
 const LoginRoutes = () => {
+	const { token } = useContext(Context);
 	const route = useRouteMatch();
+	const history = useHistory();
+	const { login } = useContext(Context);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		setLoading(true);
+		const tokenStorage = JSON.parse(window.localStorage.getItem('token'));
+		if (!tokenStorage) {
+			setLoading(false);
+			return;
+		}
+		login(tokenStorage.acessToken, tokenStorage.refreshToken);
+	}, []);
+
+	useEffect(() => {
+		if (!token?.acessToken) {
+			return;
+		}
+		window.localStorage.setItem('token', JSON.stringify(token));
+		history.replace('/dashboard');
+		setLoading(false);
+	}, [token]);
+
 	return (
 		<ContainerLogin>
-			<div>
-				<img src={logo} alt="logo" />
-			</div>
-			<Switch>
-				<Route exact path={`${route.path}`} component={Login} />
-				<Route
-					exact
-					path={`${route.path}forgetPassword`}
-					component={ForgetPassword}
-				/>
-				<Route exact path={`${route.path}register`} component={Register} />
-				<Route
-					exact
-					path={`${route.path}desactiveSucess`}
-					component={DesactiveSucess}
-				/>
-			</Switch>
+			{!loading && (
+				<>
+					<div>
+						<img src={logo} alt="logo" />
+					</div>
+					<Switch>
+						<Route exact path={`${route.path}`} component={Login} />
+						<Route
+							exact
+							path={`${route.path}forgetPassword`}
+							component={ForgetPassword}
+						/>
+						<Route exact path={`${route.path}register`} component={Register} />
+						<Route
+							exact
+							path={`${route.path}desactiveSucess`}
+							component={DesactiveSucess}
+						/>
+					</Switch>
+				</>
+			)}
 		</ContainerLogin>
 	);
 };
