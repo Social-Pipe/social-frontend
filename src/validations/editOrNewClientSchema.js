8b924a1404c4039523d8f8c7ec3d40069b9e1db1
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-06-02 11:33:23 -0300

    feat(validation): add editOrNewClientSchema

---
+++ src/validations/editOrNewClientSchema.js
@@ -0,0 +1,19 @@
+import * as Yup from 'yup';
+
+export default Yup.object().shape({
+	edit: Yup.boolean().required(),
+	name: Yup.string().when('edit', {
+		is: false,
+		then: Yup.string().required(),
+		otherwise: Yup.string(),
+	}),
+	password: Yup.string().when('edit', {
+		is: false,
+		then: Yup.string().required(),
+		otherwise: Yup.string(),
+	}),
+	instagram: Yup.boolean().required(),
+	facebook: Yup.boolean().required(),
+	linkedin: Yup.boolean().required(),
+	logo: Yup.mixed(),
+});
