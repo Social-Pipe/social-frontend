102f0836b5513580865715b8517858ef262c7633
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-09-13 12:24:45 -0300

    feat(hooks): "add error cep when error of network

--- src/hooks/useCep.js
+++ src/hooks/useCep.js
@@ -25,25 +25,29 @@ const useCep = currentCep => {
 		if (cep.length < 8) {
 			return;
 		}
-		axios.get(`https://viacep.com.br/ws/${cep}/json/`).then(({ data }) => {
-			if (data?.erro) {
-				setError(true);
-				return;
-			}
-			setCepValues(props => ({
-				...props,
-				cep: data.cep,
-				logradouro: data.logradouro,
-				ibge: data.ibge,
-				gia: data.gia,
-				ddd: data.ddd,
-				siafi: data.siafi,
-				neighborhood: data.bairro,
-				complement: data.complemento,
-				state: data.uf,
-				city: data.localidade,
-			}));
-		});
+
+		axios
+			.get(`https://viacep.com.br/ws/${cep}/json/`)
+			.then(({ data }) => {
+				if (data?.erro) {
+					setError(true);
+					return;
+				}
+				setCepValues(props => ({
+					...props,
+					cep: data.cep,
+					logradouro: data.logradouro,
+					ibge: data.ibge,
+					gia: data.gia,
+					ddd: data.ddd,
+					siafi: data.siafi,
+					neighborhood: data.bairro,
+					complement: data.complemento,
+					state: data.uf,
+					city: data.localidade,
+				}));
+			})
+			.catch(() => setError(true));
 	}, [currentCep]);
 
 	return [cepValues, error];
